513d8e83a4d5862dc43980b0b73ed8937d79388f
Kutujaya <77893381+kutu48@users.noreply.github.com>
2024-05-17 14:00:32 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 'your_api_id'
+API_HASH = 'your_api_hash'
+WORKDIR = 'your_workdir'
+BASE_URL = 'https://telegram.blum.codes'
+COOKIE = 'mp_a663ec3881444e996e51121d5a98ce4d_mixpanel=%7B%22distinct_id%22%3A%20%22e8cff157-b660-4344-b047-759d6af9ef18%22%2C%22%24device_id%22%3A%20%2218f853b931eef61-0a934f78cf0738-4c657b58-15f900-18f853b9332ef75%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%2C%22%24user_id%22%3A%20%22e8cff157-b660-4344-b047-759d6af9ef18%22%7D'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'
+ACC_DELAY = [1, 3]  # Random delay between actions in seconds
